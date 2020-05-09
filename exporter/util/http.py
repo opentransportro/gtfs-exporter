91b44a107dcabd15ac79e154203e79e57ba42caf
Vlad Vesa <hello@vladvesa.ro>
2020-05-09 21:30:31 +0300

    tweaked http util

--- exporter/util/http.py
+++ exporter/util/http.py
@@ -1,18 +1,20 @@
 import logging
-
+import time
 import requests
 
 from exporter.util.perf import measure_execution_time
 
-logger = logging.getLogger("gtfsexporter")
-
-shared_session = requests.Session()
-
 
 class Request(object):
-    def __init__(self, url):
+    def __init__(self, url: str, logger = None):
         self._url = url
         self.encoding = 'utf-8'
+        if logger is None:
+            self.logger = logging.getLogger("gtfsexporter")
+        else:
+            self.logger = logger
+
+        self.request_shared_session = requests.Session()
 
     @property
     def url(self):
@@ -21,12 +23,11 @@ class Request(object):
     # This construct allows objects to be called as functions in python
     @measure_execution_time
     def __call__(self, *args):
-        request_url = self.url.format(*args)
+        request_url = self._url.format(*args)
 
-        logger.debug(f"requesting data from ${request_url}")
-        # ts = time.time()
-        response = shared_session.get(request_url, headers={'Content-Type': 'application/json'})
-        # te = time.time()
-        # logger.info('%r took %2.2f ms' % (request_url, (te - ts) * 1000))
+        ts = time.time()
+        response = self.request_shared_session.get(request_url, headers={'Content-Type': 'application/json'})
+        te = time.time()
+        self.logger.debug('%r took %2.2f ms' % (request_url, (te - ts) * 1000))
 
         return response.json()
