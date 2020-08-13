9413ed789443c962e81eb0f77ea9fe0cb35165ba
Vlad Vesa <hello@vladvesa.ro>
2020-08-13 10:10:38 +0300

    Add route color processor and remove dead code

--- exporter/static/processors.py
+++ exporter/static/processors.py
@@ -1,8 +1,6 @@
 import logging
 import os
 
-import docker as docker
-
 from exporter.gtfs.dao import Dao
 from exporter.util.perf import run_command, check_utility
 from exporter import __map_path__
@@ -14,6 +12,35 @@ logger = logging.getLogger("gtfsexporter")
 class Processor:
     def process(self, dao: Dao):
         pass
+
+
+class RouteColorProcessor(Processor):
+    def process(self, dao: Dao):
+        logger.info("Processing data from provided source")
+
+        # Here we should use a rule providers to have more flexibility when processing data
+        # providers.process(ruleset)
+
+        for route in dao.routes():
+            print("updating route [%s] setting correct color" % route.route_long_name)
+
+            route.route_text_color = "FFFFFF"
+
+            from exporter.gtfs.model import Route
+            if route.route_type == Route.TYPE_BUS:
+                route.route_color = "195BAD"
+            elif route.route_type == Route.TYPE_TRAM:
+                route.route_color = "FFAD33"
+            elif route.route_type == Route.TYPE_RAIL:
+                route.route_color = "FF5B33"
+            elif route.route_type == Route.TYPE_CABLECAR:
+                route.route_color = "FF8433"
+            elif route.route_type == Route.TYPE_SUBWAY:
+                route.route_color = "D13333"
+            elif route.route_type == Route.TYPE_FERRY:
+                route.route_color = "62A9DD"
+
+        dao.commit()
 
 
 class OSRMTimeProcessor(Processor):
@@ -59,7 +86,6 @@ class OSRMTimeProcessor(Processor):
                      "osrm-routed", "--algorithm=MLD", "/data/map.osrm"
                      ])
 
-
     @staticmethod
     def download_file(file_names: list):
         for file_name in file_names:
@@ -75,7 +101,8 @@ class OSRMTimeProcessor(Processor):
                         f"downloading from https://github.com/opentransportro/osrm-maps/releases/download/latest/{file_name}")
                     import requests
                     file = requests.get(
-                        f"https://github.com/opentransportro/osrm-maps/releases/download/latest/{file_name}", stream=True)
+                        f"https://github.com/opentransportro/osrm-maps/releases/download/latest/{file_name}",
+                        stream=True)
 
                     with open(downloaded_file, "wb") as exported_file:
                         total_length = int(file.headers.get('content-length'))
